b54ad3ead8671fb359998300f2f3b501efe71154
Musti <test@test.com>
2026-03-16 16:48:01 +0300

    Remove animations, new fonts (Cormorant Garamond + Quicksand)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        display: ["Cinzel", "serif"],
-        body: ["Raleway", "sans-serif"],
+        display: ["Cormorant Garamond", "serif"],
+        body: ["Quicksand", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
